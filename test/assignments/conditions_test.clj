8ad8649201893c27982c64f5d2b8d822c083dedb
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2020-01-16 08:58:08 +0530

    Adding a sample test and setting kaocha config

---
+++ test/assignments/conditions_test.clj
@@ -0,0 +1,9 @@
+(ns assignments.conditions-test
+  (:require [clojure.test :refer :all]
+            [assignments.conditions :refer :all]))
+
+(deftest safe-division
+  (testing "non zero denominator"
+    (is (= 2 (safe-divide 4 2))))
+  (testing "zero denominator"
+    (is (nil? (safe-divide 3 0)))))
