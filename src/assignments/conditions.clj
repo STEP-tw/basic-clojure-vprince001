15284634e758ea16758ba728155874f7834ac50b
Prince Verma <16836599+vprince001@users.noreply.github.com>
2020-02-03 01:49:07 +0530

    implement zero-aliases

--- src/assignments/conditions.clj
+++ src/assignments/conditions.clj
@@ -101,8 +101,15 @@
   \"\"  -> :empty-string"
   {:level        :easy
    :use          '[case]
-   :implemented? false}
-  [zero-like-value])
+   :implemented? true}
+  [zero-like-value] (case zero-like-value
+                      0 :zero
+                      [] :empty
+                      `() :empty
+                      #{} :empty-set
+                      {} :empty-map
+                      "" :empty-string
+                      :not-zero))
 
 (defn zero-separated-palindrome
   "Given a sequence of numbers, increment the list
